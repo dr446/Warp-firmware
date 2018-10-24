cf677a9589cbad220e423f8e71854dbab9fcd762
Diya <dr446@cam.ac.uk>
2018-10-24 10:24:11 +0100

    my changes to original

---
+++ build/ksdk1.1/work/demos/Warp/armgcc/Warp/CMakeFiles/Warp.dir/home/diya/Documents/Cambridge_Engineering/4b25/Warp-firmware/build/ksdk1.1/work/demos/Warp/src/devSSD1331.c.d
@@ -0,0 +1,68 @@
+CMakeFiles/Warp.dir/home/diya/Documents/Cambridge_Engineering/4b25/Warp-firmware/build/ksdk1.1/work/demos/Warp/src/devSSD1331.c.obj: \
+ /home/diya/Documents/Cambridge_Engineering/4b25/Warp-firmware/build/ksdk1.1/work/demos/Warp/src/devSSD1331.c \
+ /home/diya/Documents/Cambridge_Engineering/4b25/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/../../../../platform/drivers/inc/fsl_spi_master_driver.h \
+ /home/diya/Documents/Cambridge_Engineering/4b25/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/../../../../platform/hal/inc/fsl_spi_hal.h \
+ /home/diya/Documents/Cambridge_Engineering/4b25/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/../../../../platform/CMSIS/Include/device/fsl_device_registers.h \
+ /home/diya/Documents/Cambridge_Engineering/4b25/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/../../../../platform/CMSIS/Include/device/MKL03Z4/MKL03Z4.h \
+ /home/diya/Documents/Cambridge_Engineering/4b25/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/../../../../platform/CMSIS/Include/core_cm0plus.h \
+ /home/diya/Documents/Cambridge_Engineering/4b25/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/../../../../platform/CMSIS/Include/core_cmInstr.h \
+ /home/diya/Documents/Cambridge_Engineering/4b25/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/../../../../platform/CMSIS/Include/core_cmFunc.h \
+ /home/diya/Documents/Cambridge_Engineering/4b25/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/../../../../platform/startup/MKL03Z4/system_MKL03Z4.h \
+ /home/diya/Documents/Cambridge_Engineering/4b25/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/../../../../platform/CMSIS/Include/device/MKL03Z4/MKL03Z4_registers.h \
+ /home/diya/Documents/Cambridge_Engineering/4b25/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/../../../../platform/CMSIS/Include/device/MKL03Z4/MKL03Z4.h \
+ /home/diya/Documents/Cambridge_Engineering/4b25/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/../../../../platform/CMSIS/Include/device/MKL03Z4/fsl_bitaccess.h \
+ /home/diya/Documents/Cambridge_Engineering/4b25/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/../../../../platform/CMSIS/Include/device/MKL03Z4/MKL03Z4_features.h \
+ /home/diya/Documents/Cambridge_Engineering/4b25/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/../../../../platform/osa/inc/fsl_os_abstraction.h \
+ /home/diya/Documents/Cambridge_Engineering/4b25/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/../../../../platform/osa/inc/fsl_os_abstraction_bm.h \
+ /home/diya/Documents/Cambridge_Engineering/4b25/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/../../../../platform/hal/inc/fsl_port_hal.h \
+ /home/diya/Documents/Cambridge_Engineering/4b25/Warp-firmware/build/ksdk1.1/work/demos/Warp/src/SEGGER_RTT.h \
+ /home/diya/Documents/Cambridge_Engineering/4b25/Warp-firmware/build/ksdk1.1/work/demos/Warp/src/SEGGER_RTT_Conf.h \
+ /home/diya/Documents/Cambridge_Engineering/4b25/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/../../../../boards/Warp/gpio_pins.h \
+ /home/diya/Documents/Cambridge_Engineering/4b25/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/../../../../platform/drivers/inc/fsl_gpio_driver.h \
+ /home/diya/Documents/Cambridge_Engineering/4b25/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/../../../../platform/hal/inc/fsl_gpio_hal.h \
+ /home/diya/Documents/Cambridge_Engineering/4b25/Warp-firmware/build/ksdk1.1/work/demos/Warp/src/warp.h \
+ /home/diya/Documents/Cambridge_Engineering/4b25/Warp-firmware/build/ksdk1.1/work/demos/Warp/src/devSSD1331.h
+
+/home/diya/Documents/Cambridge_Engineering/4b25/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/../../../../platform/drivers/inc/fsl_spi_master_driver.h:
+
+/home/diya/Documents/Cambridge_Engineering/4b25/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/../../../../platform/hal/inc/fsl_spi_hal.h:
+
+/home/diya/Documents/Cambridge_Engineering/4b25/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/../../../../platform/CMSIS/Include/device/fsl_device_registers.h:
+
+/home/diya/Documents/Cambridge_Engineering/4b25/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/../../../../platform/CMSIS/Include/device/MKL03Z4/MKL03Z4.h:
+
+/home/diya/Documents/Cambridge_Engineering/4b25/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/../../../../platform/CMSIS/Include/core_cm0plus.h:
+
+/home/diya/Documents/Cambridge_Engineering/4b25/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/../../../../platform/CMSIS/Include/core_cmInstr.h:
+
+/home/diya/Documents/Cambridge_Engineering/4b25/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/../../../../platform/CMSIS/Include/core_cmFunc.h:
+
+/home/diya/Documents/Cambridge_Engineering/4b25/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/../../../../platform/startup/MKL03Z4/system_MKL03Z4.h:
+
+/home/diya/Documents/Cambridge_Engineering/4b25/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/../../../../platform/CMSIS/Include/device/MKL03Z4/MKL03Z4_registers.h:
+
+/home/diya/Documents/Cambridge_Engineering/4b25/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/../../../../platform/CMSIS/Include/device/MKL03Z4/MKL03Z4.h:
+
+/home/diya/Documents/Cambridge_Engineering/4b25/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/../../../../platform/CMSIS/Include/device/MKL03Z4/fsl_bitaccess.h:
+
+/home/diya/Documents/Cambridge_Engineering/4b25/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/../../../../platform/CMSIS/Include/device/MKL03Z4/MKL03Z4_features.h:
+
+/home/diya/Documents/Cambridge_Engineering/4b25/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/../../../../platform/osa/inc/fsl_os_abstraction.h:
+
+/home/diya/Documents/Cambridge_Engineering/4b25/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/../../../../platform/osa/inc/fsl_os_abstraction_bm.h:
+
+/home/diya/Documents/Cambridge_Engineering/4b25/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/../../../../platform/hal/inc/fsl_port_hal.h:
+
+/home/diya/Documents/Cambridge_Engineering/4b25/Warp-firmware/build/ksdk1.1/work/demos/Warp/src/SEGGER_RTT.h:
+
+/home/diya/Documents/Cambridge_Engineering/4b25/Warp-firmware/build/ksdk1.1/work/demos/Warp/src/SEGGER_RTT_Conf.h:
+
+/home/diya/Documents/Cambridge_Engineering/4b25/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/../../../../boards/Warp/gpio_pins.h:
+
+/home/diya/Documents/Cambridge_Engineering/4b25/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/../../../../platform/drivers/inc/fsl_gpio_driver.h:
+
+/home/diya/Documents/Cambridge_Engineering/4b25/Warp-firmware/build/ksdk1.1/work/demos/Warp/armgcc/Warp/../../../../platform/hal/inc/fsl_gpio_hal.h:
+
+/home/diya/Documents/Cambridge_Engineering/4b25/Warp-firmware/build/ksdk1.1/work/demos/Warp/src/warp.h:
+
+/home/diya/Documents/Cambridge_Engineering/4b25/Warp-firmware/build/ksdk1.1/work/demos/Warp/src/devSSD1331.h:
